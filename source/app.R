e043dec0805e4f27dad74f29934e9651004e69bd
Roman Hillje <roman.hillje@googlemail.com>
2019-04-23 16:30:28 +0200

    Clean up code.

--- source/app.R
+++ source/app.R
@@ -1,45 +1,4 @@
 ##----------------------------------------------------------------------------##
-## Cerebro
-## version 1.0.0
-##
-## Author:    Roman Hillje
-## Institute: IEO
-## Lab:       PGP
-## Date:      2019-04-01
-##----------------------------------------------------------------------------##
-
-##----------------------------------------------------------------------------##
-##
-##----------------------------------------------------------------------------##
-if (grepl(tolower(Sys.info()["sysname"]), pattern="^win")) {
-  .libPaths(paste0(getwd(), "/R-Portable-Win/library"))
-  plot_export_path <- paste0(Sys.getenv("USERPROFILE"), "\\Desktop\\")
-} else {
-  .libPaths(paste0(getwd(), "/R-Portable-Mac/library"))
-  plot_export_path <- "~/Desktop/"
-}
-
-##----------------------------------------------------------------------------##
-##
-##----------------------------------------------------------------------------##
-if ( !is.element(el = "BiocManager", set = rownames(installed.packages())) ) {
-  install.packages(
-    "BiocManager",
-    repos = "http://cran.us.r-project.org",
-    dependencies = TRUE
-  )
-}
-
-##----------------------------------------------------------------------------##
-##
-##----------------------------------------------------------------------------##
-if ( !is.element(el = "cerebroApp", set = rownames(installed.packages())) ) {
-  BiocManager::install(
-    "romanhaa/cerebroApp"
-  )
-}
-
-##----------------------------------------------------------------------------##
-##
+## Launch Cerebro from cerebroApp package.
 ##----------------------------------------------------------------------------##
 cerebroApp::launchApp()
--- source/main.js
+++ source/main.js
@@ -1,4 +1,4 @@
-// Modules to control application life and create native browser window
+// modules to control application life and create native browser window
 const { app, BrowserWindow, Menu, MenuItem } = require("electron")
 
 const path = require("path")
@@ -12,13 +12,11 @@ var killStr = ""
 var appPath = path.join(app.getAppPath(), "app.R" )
 var execPath = "RScript"
 
-// log file
 var log = require("electron-log")
 log.transports.console.level = true
 log.transports.file.level = true
 log.transports.file.level = "info"
 
-//
 if ( process.platform == WINDOWS ) {
   appPath = appPath.replace(/\\/g, "\\\\")
   execPath = path.join(app.getAppPath(), "R-Portable-Win", "bin", "RScript.exe" )
@@ -35,10 +33,8 @@ if ( process.platform == WINDOWS ) {
   throw new Error("Not on Windows or macOS?")
 }
 
-//
 log.info("Environment:\n", process.env)
 
-//
 const childProcess = child.spawn(execPath, ["--vanilla", "-e", "shiny::runApp(file.path('" + appPath + "'), port=" + port + ")"])
 childProcess.stdout.on("data", (data) => {
   log.info("stdout:\n", `${data}`)
@@ -47,11 +43,9 @@ childProcess.stderr.on("data", (data) => {
   log.info("stderr:\n", `${data}`)
 })
 
-// Keep a global reference of the window object, if you don"t, the window will
-// be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
-// Create the browser window.
+//
 function createWindow () {
   log.info("Working directory: " + process.cwd())
   log.info("create-window")
@@ -90,28 +84,9 @@ function createWindow () {
 
     log.info("Using port "+port)
 
-    // long loading html
     mainWindow.loadURL("http://127.0.0.1:" + port)
-    
-    // mainWindow.webContents.on("did-finish-load", function() {
-    //   log.info("did-finish-load")
-    // })
 
-    // mainWindow.webContents.on("did-start-load", function() {
-    //   log.info("did-start-load")
-    // })
-
-    // mainWindow.webContents.on("did-stop-load", function() {
-    //   log.info("did-stop-load")
-    // })
-
-    // mainWindow.webContents.on("dom-ready", function() {
-    //   log.info("dom-ready")
-    // })
-
-    // Emitted when the window is closed.
     mainWindow.on("closed", function () {
-      log.info("mainWindow.closed()")
       cleanUpApplication()
     })
 
@@ -169,27 +144,20 @@ function setMainMenu() {
   Menu.setApplicationMenu(Menu.buildFromTemplate(template))
 }
 
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
+//
 app.on("ready", function () {
   createWindow()
   setMainMenu()
 })
 
-// Quit when all windows are closed.
+//
 app.on("window-all-closed", function () {
   log.info("window-all-closed")
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
   cleanUpApplication()
 })
 
 //
 app.on("activate", function () {
-  // On OS X it"s common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if ( mainWindow === null ) {
     createWindow()
   }
